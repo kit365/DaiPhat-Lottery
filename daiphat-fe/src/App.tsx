0281ae58e6f92bf95c4c8f949f10011920585c1f
Bui Anh Dung <buianh09dung@gmail.com>
2026-05-19 18:00:46 +0700

    [dp-5] feat: template blog  #done

--- daiphat-fe/src/App.tsx
+++ daiphat-fe/src/App.tsx
@@ -8,6 +8,8 @@ import { PrivateRoute } from './client/pages/private.route';
 import { ProfilePage as ClientProfilePage } from './client/pages/profile/ProfilePage';
 import { ProfileDashboardPage } from './client/pages/profile/ProfileDashboardPage';
 
+import { BlogListPage } from './client/pages/BlogListPage';
+
 import { ROUTES } from './admin/constants/routes';
 import { LoadingSpinner } from './client/components/ui/LoadingSpinner';
 import { AuthGuard } from './admin/components/auth/AuthGuard';
@@ -42,20 +44,21 @@ function App() {
         {/* Client Side Theme Context */}
         <Route element={<ClientThemeLayout />}>
           <Route path="/" element={<HomePage />} />
-          
+          <Route path="/blogs" element={<BlogListPage />} />
+
           {/* Mapping Common Routes into Client Theme */}
           {CommonRoutes.map(({ path, element }: any) => (
             <Route key={path} path={path} element={element} />
           ))}
-          
+
           <Route path="/profile" element={<PrivateRoute />}>
             <Route index element={<ClientProfilePage />} />
           </Route>
-          
+
           <Route path="/profile-v2" element={<PrivateRoute />}>
             <Route index element={<ProfileDashboardPage />} />
           </Route>
-          
+
           <Route path="*" element={<Navigate to="/" replace />} />
 
         </Route>
@@ -75,15 +78,15 @@ function App() {
             {/* Admin Authenticated Areas */}
             <Route element={<AuthGuard><LayoutAdmin /></AuthGuard>}>
               {AdminRoutes.map(({ path, element, index }: any) => (
-                <Route 
-                  key={path || (index ? "index" : "unknown")} 
-                  path={path} 
-                  index={index} 
-                  element={element} 
+                <Route
+                  key={path || (index ? "index" : "unknown")}
+                  path={path}
+                  index={index}
+                  element={element}
                 />
               ))}
             </Route>
-            
+
             {/* Common Routes inside Admin Context */}
             {/* 1. Unguarded Common Routes (Callback) */}
             <Route path="auth/callback" element={<OAuthCallbackPage />} />
