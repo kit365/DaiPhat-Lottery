e73cbd4c2d56c457a17a1f7aebffc5d5e02fb53d
Kiet Tuan <kietops365@gmail.com>
2026-06-05 10:46:49 +0700

    [DP-322] feat: implement Redis ZSet-backed scheduled blog publishing queue and post scheduling UI #done

---
+++ daiphat-be/core-api/src/main/java/com/daiphat/coreapi/infrastructure/scheduler/BlogPublishScheduler.java
@@ -0,0 +1,25 @@
+package com.daiphat.coreapi.infrastructure.scheduler;
+
+import com.daiphat.coreapi.application.port.in.blog.BlogPostServicePort;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class BlogPublishScheduler {
+
+    private final BlogPostServicePort blogPostServicePort;
+
+    @Scheduled(fixedRateString = "${daiphat.blog.schedule-publish-rate-ms:30000}")
+    public void publishScheduledPosts() {
+        int publishedCount = blogPostServicePort.publishDueScheduledPosts();
+        if (publishedCount > 0) {
+            log.info("Published {} scheduled blog posts", publishedCount);
+        } else {
+            log.debug("No scheduled blog posts due for publishing");
+        }
+    }
+}
---
+++ daiphat-be/core-api/src/main/java/com/daiphat/coreapi/shared/util/StatusCountKeys.java
@@ -0,0 +1,9 @@
+package com.daiphat.coreapi.shared.util;
+
+public final class StatusCountKeys {
+
+    public static final String ALL = "all";
+
+    private StatusCountKeys() {
+    }
+}
