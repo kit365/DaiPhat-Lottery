d1a161da28bf96fe8e3fc7240ac217a7aea0296d
Duc Long <longntdse181516@fpt.edu.vn>
2026-06-07 09:18:41 +0700

    [dp-390] feat:  UI xem danh sách vé

---
+++ daiphat_mobile/lib/data/models/lottery_result.dart
@@ -0,0 +1,13 @@
+class LotteryResult {
+  final String id;
+  final String date;
+  final String province;
+  final String specialPrize;
+
+  LotteryResult({
+    required this.id,
+    required this.date,
+    required this.province,
+    required this.specialPrize,
+  });
+}
