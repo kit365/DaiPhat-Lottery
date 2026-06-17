cf6c5f5fd6905dba89476f7db9fb529a7d58a403
Phuoc <phuocnguyenhuu2510@gmail.com>
2026-06-17 14:55:32 +0700

    [DP-543] feat: implement UI and handle create street vendor account API #done

--- daiphat-be/core-api/src/main/java/com/daiphat/coreapi/application/port/out/streetagent/StreetAgentProfileRepositoryPort.java
+++ daiphat-be/core-api/src/main/java/com/daiphat/coreapi/application/port/out/streetagent/StreetAgentProfileRepositoryPort.java
@@ -1,9 +1,13 @@
 package com.daiphat.coreapi.application.port.out.streetagent;
 
+import com.daiphat.coreapi.domain.model.enums.streetagent.StreetAgentProfileStatus;
 import com.daiphat.coreapi.domain.model.streetagent.StreetAgentProfileModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface StreetAgentProfileRepositoryPort {
     StreetAgentProfileModel save(StreetAgentProfileModel profile);
     boolean existsByPhone(String phone);
     boolean existsByCccd(String cccd);
+    Page<StreetAgentProfileModel> findAll(Pageable pageable, String search, StreetAgentProfileStatus status);
 }
