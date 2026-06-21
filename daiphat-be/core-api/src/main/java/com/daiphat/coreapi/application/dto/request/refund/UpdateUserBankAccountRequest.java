61b3ba2ec9af1fc6e0b281db958c40277e0846a9
Phu Dep Trai <phujason1992@gmail.com>
2026-06-21 19:25:36 +0700

    [DP-361] feat: add refund terms acceptance validation to bank account requests and update error codes

--- daiphat-be/core-api/src/main/java/com/daiphat/coreapi/application/dto/request/refund/UpdateUserBankAccountRequest.java
+++ daiphat-be/core-api/src/main/java/com/daiphat/coreapi/application/dto/request/refund/UpdateUserBankAccountRequest.java
@@ -1,12 +1,15 @@
 package com.daiphat.coreapi.application.dto.request.refund;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UpdateUserBankAccountRequest(
         @NotBlank @Size(max = 20) String bankBin,
         @NotBlank @Size(max = 50) String bankAccountNo,
         @NotBlank @Size(max = 150) String bankAccountName,
-        Boolean isDefault
+        Boolean isDefault,
+        @NotNull @AssertTrue Boolean agreedToRefundTerms
 ) {
 }
