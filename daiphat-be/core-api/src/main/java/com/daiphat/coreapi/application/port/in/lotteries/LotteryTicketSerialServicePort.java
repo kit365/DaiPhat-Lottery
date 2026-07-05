490b858d639dfe88f82c53c85ec58103adcdd75f
Phu Dep Trai <phujason1992@gmail.com>
2026-07-05 11:44:27 +0700

    [DP-628] feat: update ticket create/save APIs to use import batch via serial #in-progress

--- daiphat-be/core-api/src/main/java/com/daiphat/coreapi/application/port/in/lotteries/LotteryTicketSerialServicePort.java
+++ daiphat-be/core-api/src/main/java/com/daiphat/coreapi/application/port/in/lotteries/LotteryTicketSerialServicePort.java
@@ -21,7 +21,9 @@ public interface LotteryTicketSerialServicePort {
     LotteryTicketSerialModel upsertSerialForTicket(
             LotteryTicketModel ticket,
             CreateLotteryTicketSerialRequest request,
-            UUID importedById
+            UUID importedById,
+            Long importBatchId,
+            Long importBatchLineId
     );
 
     void syncSerialsForTicket(
@@ -55,6 +57,8 @@ public interface LotteryTicketSerialServicePort {
     List<LotteryTicketSerialModel> findAllByTicketId(Long ticketId);
 
     long countByImportBatchLineId(Long importBatchLineId);
+
+    List<Long> findDistinctTicketIdsByImportBatchLineId(Long importBatchLineId);
 
     List<EnumOptionResponse> getStatuses();
 }
