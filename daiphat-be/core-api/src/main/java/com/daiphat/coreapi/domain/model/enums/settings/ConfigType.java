5edbac5bc010d19b08072c186fc20ad8ca754b61
Phu Dep Trai <phujason1992@gmail.com>
2026-07-17 23:18:24 +0700

    [DP-744] feat: implement auto-close functionality for resolved support tickets and add complaint settings #done

--- daiphat-be/core-api/src/main/java/com/daiphat/coreapi/domain/model/enums/settings/ConfigType.java
+++ daiphat-be/core-api/src/main/java/com/daiphat/coreapi/domain/model/enums/settings/ConfigType.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 public enum ConfigType implements LabeledEnum {
     ORDER_SETTING("Cấu hình đơn hàng"),
     TICKET_IMPORT("Cấu hình nhập vé"),
-    REFUND_SETTING("Cấu hình hoàn tiền");
+    REFUND_SETTING("Cấu hình hoàn tiền"),
+    COMPLAINT_SETTING("Cấu hình khiếu nại");
 
     private final String label;
 }
