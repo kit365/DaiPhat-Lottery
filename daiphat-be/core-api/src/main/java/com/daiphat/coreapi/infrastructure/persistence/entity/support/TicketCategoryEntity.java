42d6b0bbd64075e83ac31c407d8e524ef12521da
Phu Dep Trai <phujason1992@gmail.com>
2026-07-19 00:23:19 +0700

    [DP-763] feat: add isActive field to ticket categories and update related SQL scripts #done

--- daiphat-be/core-api/src/main/java/com/daiphat/coreapi/infrastructure/persistence/entity/support/TicketCategoryEntity.java
+++ daiphat-be/core-api/src/main/java/com/daiphat/coreapi/infrastructure/persistence/entity/support/TicketCategoryEntity.java
@@ -44,6 +44,10 @@ public class TicketCategoryEntity {
     @Column(name = "parent_id")
     private Long parentId;
 
+    @Column(name = "is_active", nullable = false)
+    @Builder.Default
+    private boolean isActive = true;
+
     @CreatedDate
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
