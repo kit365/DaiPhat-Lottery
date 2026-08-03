f7f0aa08c78cbb28b76d3f8032efea24b8c1228a
Phuoc <phuocnguyenhuu2510@gmail.com>
2026-08-03 21:32:31 +0700

    [DP-37] fix: suppress root html/body hydration warnings from extensions

--- daiphat-fe/src/app/layout.tsx
+++ daiphat-fe/src/app/layout.tsx
@@ -38,12 +38,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="vi" className={`${inter.variable} ${barlow.variable} ${publicSans.variable}`}>
+    <html
+      lang="vi"
+      className={`${inter.variable} ${barlow.variable} ${publicSans.variable}`}
+      suppressHydrationWarning
+    >
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
-      <body className="antialiased">
+      <body className="antialiased" suppressHydrationWarning>
         {/* Icon fonts loaded non-blocking after page is interactive */}
         <FontLoader />
         <Providers>{children}</Providers>
