b68ae14f06f684169089361f04b19f8c1118e669
Phu Dep Trai <phujason1992@gmail.com>
2026-08-05 19:08:16 +0700

    [DP-919] chore: update NDK version and adjust Kotlin plugin version in build configuration #done

--- daiphat_mobile/android/app/build.gradle.kts
+++ daiphat_mobile/android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.daiphat_mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
